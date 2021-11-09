2de38bc6f50393dbf75c9d9c58fb3beda785333e
moik.oleksandr <moik.o@hexide-digital.com>
2021-11-09 23:37:49 +0200

    remove role and permission models from admin panel package

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -5,7 +5,20 @@ namespace HexideDigital\ModelPermissions\Models;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property string title
+ * App\Models\Permission
+ *
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string|null $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Permission whereUpdatedAt($value)
  */
 class Permission extends Model
 {
--- src/Models/Role.php
+++ src/Models/Role.php
@@ -6,9 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
- * @property string title
- * @property string|null key
- * @property bool admin_access
+ * App\Models\Role
+ *
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $title
+ * @property string|null $key
+ * @property int $admin_access
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\HexideDigital\ModelPermissions\Models\Permission[] $permissions
+ * @property-read int|null $permissions_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Role newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereAdminAccess($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereKey($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereUpdatedAt($value)
  */
 class Role extends Model
 {
