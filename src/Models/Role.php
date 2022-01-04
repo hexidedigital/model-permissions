6934c39c5865995de0b5217f23d6ea20f9e4f516
moik.olesandr <moik.o@hexide-digital.com>
2022-01-04 16:02:15 +0200

    update services

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -2,45 +2,40 @@
 
 namespace HexideDigital\ModelPermissions\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Carbon;
 
 /**
- * App\Models\Role
- *
  * @mixin \Eloquent
  * @property int $id
  * @property string $title
  * @property string|null $key
  * @property int $admin_access
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection|\HexideDigital\ModelPermissions\Models\Permission[] $permissions
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection|Permission[] $permissions
  * @property-read int|null $permissions_count
- * @method static \Illuminate\Database\Eloquent\Builder|Role newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Role newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Role query()
- * @method static \Illuminate\Database\Eloquent\Builder|Role whereAdminAccess($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Role whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Role whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Role whereKey($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Role whereTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Role whereUpdatedAt($value)
+ * @method static Builder|Role newModelQuery()
+ * @method static Builder|Role newQuery()
+ * @method static Builder|Role query()
+ * @method static Builder|Role whereAdminAccess($value)
+ * @method static Builder|Role whereCreatedAt($value)
+ * @method static Builder|Role whereId($value)
+ * @method static Builder|Role whereKey($value)
+ * @method static Builder|Role whereTitle($value)
+ * @method static Builder|Role whereUpdatedAt($value)
  */
 class Role extends Model
 {
-    /**
-     * @var string[]
-     */
     protected $fillable = [
         'title',
         'key',
         'admin_access',
     ];
 
-    /**
-     * @return BelongsToMany
-     */
     public function permissions(): BelongsToMany
     {
         return $this->belongsToMany(Permission::class);
